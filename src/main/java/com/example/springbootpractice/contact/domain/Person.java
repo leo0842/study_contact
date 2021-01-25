36ee0e07db00a7e78783f24ab872225ee41f0c0a
sungsan <kunga077@gmail.com>
2021-01-25 16:20:29 +0900

    Lombok 학습

--- src/main/java/com/example/springbootpractice/contact/domain/Person.java
+++ src/main/java/com/example/springbootpractice/contact/domain/Person.java
@@ -1,2 +1,43 @@
-package com.example.springbootpractice.contact.domain;public class Person {
+package com.example.springbootpractice.contact.domain;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Person {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private String name;
+
+  private int age;
+
+  @NonNull
+  private String hobby;
+
+  private String bloodType;
+
+  private String address;
+
+  private LocalDate birthday;
+
+  private String job;
+
+  @ToString.Exclude
+  private String phoneNumber;
+
 }
--- src/test/java/com/example/springbootpractice/contact/repository/PersonRepositoryTest.java
+++ src/test/java/com/example/springbootpractice/contact/repository/PersonRepositoryTest.java
@@ -1,2 +1,51 @@
-package com.example.springbootpractice.contact.repository;public class PersonRepositoryTest {
+package com.example.springbootpractice.contact.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+import com.example.springbootpractice.contact.domain.Person;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PersonRepositoryTest {
+
+  @Autowired
+  private PersonRepository personRepository;
+
+
+  @Test
+  void crud(){
+
+    Person person = new Person();
+    person.setName("Leo");
+    person.setAge(20);
+
+    personRepository.save(person);
+//    System.out.println(personRepository.findAll());
+
+    List<Person> people = personRepository.findAll();
+
+    assertEquals(people.size(),1);
+    assertEquals(people.get(0).getId(),1);
+    assertEquals(people.get(0).getName(),"Leo");
+    assertEquals(people.get(0).getAge(),20);
+  }
+
+  @Test
+  void lombokTest(){
+    Person person1 = new Person();
+    Person person2 = new Person();
+    person1.setName("Leo");
+    person2.setName("Leo");
+    person1.setAge(20);
+    person2.setAge(20);
+
+    System.out.println(person1.equals(person2));
+    System.out.println(person1.hashCode()==person2.hashCode());
+
+    System.out.println(person1);
+
+  }
+
 }
